ebf6974f8a7f1e2bc8fe90a97bcc565782b1079b
Brett Yarrow <byarrow86@gmail.com>
2019-11-20 15:39:07 -0800

    Created individual component for each Location row, uses props to fill data, styling done

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,54 @@
-<<<<<<< HEAD
-import React from 'react';
-import logo from './logo.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Location from './components/table/Location';
-
-function App() {
-  return (
-    <div className="App">
-      <Location />
-    </div>
-  );
-}
-=======
 import React, { Component } from 'react';
 import Navbar from './components/navbar/Navbar';
 import Inventory from './components/navbar/Inventory';
 import Locations from './components/navbar/Locations';
 import { BrowserRouter, Route } from 'react-router-dom';
+import Location from './components/table/Location';
 
->>>>>>> development
+const locationData = {
+  "id": "32bbed84-d450-47a6-a2f1-0648ff9c01d1",
+  "name": "Warehouse Number 1",
+  "address": "469 King St W, Toronto, ON",
+  "contact": {
+    "name": "Mara Weinberg",
+    "role": "Warehouse Manager",
+    "phone": "+1 416 678 2345",
+    "email": "mweinberg@instack.com"
+  },
+  "categories": [
+    "Industrial",
+    "Automotive",
+    "Heavy",
+    "Mechanical",
+    "Engineering",
+    "Transportation"
+  ]
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      <Location locationData={locationData} />
+    </div>
+  );
+}
 
 
-export default class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="App">
-          <Navbar />
-          <Route path="/inventory" component={Inventory} />
-          <Route path="/locations" component={Locations} />
-        </div>
-      </BrowserRouter>
-    );
-  }
-} 
+
+
+
+
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <BrowserRouter>
+//         <div className="App">
+//           <Navbar />
+//           <Route path="/inventory" component={Inventory} />
+//           <Route path="/locations" component={Locations} />
+//         </div>
+//       </BrowserRouter>
+//     );
+//   }
+// } 
 
--- client/src/components/table/Location.js
+++ client/src/components/table/Location.js
@@ -1,22 +1,33 @@
 import React from 'react'
+import rightArrow from '../../assets/icons/right-arrow.svg'
 
 export default function Location(props) {
+    const { name, address, contact, categories } = props.locationData
     return (
         <div className="location__row">
-            <div className="location__warehouse">
-                <h2 className="location__name">Warehouse Number 1</h2>
-                <h5 className="location__address">469 King St W, Toronto, ON</h5>
+            <div className="location__content">
+                <div className="location__warehouse">
+                    <h2 className="location__name">{name}</h2>
+                    <h5 className="location__address">{address}</h5>
+                </div>
+                <div className="location__subcontainer">
+                    <div className="location__contact">
+                        <h5 className="location__contact-name">{contact.name}</h5>
+                        <h5 className="location__contact-role">{contact.role}</h5>
+                    </div>
+                    <div className="location__contactinfo">
+                        <h5 className="location__contactinfo-phone">{contact.phone}</h5>
+                        <h5 className="location__contactinfo-email">{contact.email}</h5>
+                    </div>
+                    <div className="location__categories">
+                        <div className="location__line-clamp">
+                            <h5 className="location__categories-text">{categories.join(', ')}</h5>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="location__contact">
-                <h5 className="location__contact-name">Mara Weinberg</h5>
-                <h5 className="location__contact-role">Warehouse Manager</h5>
-            </div>
-            <div className="location__contact-info">
-                <h5 className="location__contact-phone">+1 416 678 2345</h5>
-                <h5 className="location__contact-email">Warehouse Manager</h5>
-            </div>
-            <div className="location__categories">
-                <h5 className="location__contact-phone">Industrial, Automotive, Heavy, Mechanical, Engineering</h5>
+            <div className="location__image-box">
+                <img src={rightArrow} alt="right arrow"></img>
             </div>
         </div>
     )
