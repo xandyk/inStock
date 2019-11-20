ea46405d9923535c9593c0410f72474ef48997e9
BFawcett1 <fawcett.ben@gmail.com>
2019-11-20 14:29:05 -0800

    started product before update

---
+++ client/src/components/table/product.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+export default function Product(props) {
+  return (
+    <div className="product">
+      <div className="product__top-container">
+        <h1 className="product__header">Inventory</h1>
+        <form>
+            <label></label>
+            <input type="text" name="search" placeholder="Search"></input>
+        </form>
+      </div>
+      <div className="product__mid-container">
+        <div>
+          <h5 className="product__label">Item</h5>
+          <h3 className="product__name">Product Name Here</h3>
+          <h4 className="product__data">
+            Here is a very brief description of the product...
+          </h4>
+        </div>
+        <div className="product__data-container">
+          <h5 className="product__label">Last Ordered</h5>
+          <h4 className="product__data">05/24/2018</h4>
+        </div>
+        <div className="product__data-container">
+          <h5 className="product__label">Location</h5>
+          <h4 className="product__data">Toronto, CAN</h4>
+        </div>
+        <div className="product__data-container">
+          <h5 className="product__label">Quantity</h5>
+          <h4 className="product__data">12,000</h4>
+        </div>
+        <div className="product__data-container">
+          <h5 className="product__label">Status</h5>
+          <h4 className="product__data">In Stock</h4>
+        </div>
+      </div>
+    </div>
+  );
+}
