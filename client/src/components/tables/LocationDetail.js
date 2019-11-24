71ed3154fda0cbff752b9d361b74416ea9a42da9
Brett Yarrow <byarrow86@gmail.com>
2019-11-24 10:54:14 -0800

    added headings, adjusted styling

--- client/src/components/tables/LocationDetail.js
+++ client/src/components/tables/LocationDetail.js
@@ -8,6 +8,7 @@ export default function LocationDetail(props) {
 
         )
     })
+    const { id, name, address, contact, categories } = locationData
     console.log(locationData)
     return (
         <div className="locationDetail">
@@ -17,8 +18,27 @@ export default function LocationDetail(props) {
                     <div className="locationDetail__backArrow-box">
                         <img className="locationDetail__backArrow" src={backArrow} />
                     </div>
-                    <h1 className="locationDetail__title">{locationData.name}</h1>
+                    <h1 className="locationDetail__title">{name}</h1>
                 </div>
+            </div>
+            <div className="locationDetail__labels">
+                <h5 className="locationDetail__labels-text">ADDRESS</h5>
+
+
+                <h5 className="locationDetail__labels-text">CONTACT</h5>
+                <div className="locationDetail__contact">
+                    <h5 className="locationDetail__contact-name">{contact.name}</h5>
+                    <h5 className="locationDetail__contact-role">{contact.role}</h5>
+                </div>
+                <div className="locationDetail__contactinfo">
+                    <h5 className="locationDetail__contactinfo-phone">{contact.phone}</h5>
+                    <h5 className="locationDetail__contactinfo-email">{contact.email}</h5>
+                </div>
+
+
+
+                <h5 className="locationDetail__labels-text">CONTACT INFORMATION</h5>
+                <h5 className="locationDetail__labels-text locationDetail__labels-text--last">CATEGORIES</h5>
             </div>
             {/* {locationTable} */}
             {/* <Add /> */}
