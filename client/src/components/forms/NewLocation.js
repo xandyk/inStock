2077bfb69055f6c12d6d8433bb1ce8b7fd5d6a7f
Brett Yarrow <byarrow86@gmail.com>
2019-11-26 21:20:53 -0800

    fixed cancel buttons on new location/product.  Were creating blank entries

--- client/src/components/forms/NewLocation.js
+++ client/src/components/forms/NewLocation.js
@@ -9,10 +9,11 @@ export default class NewLocation extends Component {
             name: event.target.name.value,
             address: event.target.address.value,
             location: event.target.countries.value,
-            contact: {name: event.target.contactname.value,
-            phone: event.target.phone.value,
-            position: event.target.position.value,
-            email: event.target.email.value
+            contact: {
+                name: event.target.contactname.value,
+                phone: event.target.phone.value,
+                position: event.target.position.value,
+                email: event.target.email.value
             },
             categories: event.target.categories.value.split(',')
         }).then(response => {
@@ -23,20 +24,20 @@ export default class NewLocation extends Component {
 
     }
     render() {
-        return(
+        return (
             <>
                 <h1 className="form__header">Add New</h1>
 
                 <form onSubmit={event => this.newFormInfo(event)} className="form__container">
                     <div className="form__row-one">
                         <h4 className="form__label">WHAREHOUSE</h4>
-                        <input className="form__box" type="text" name="name" placeholder="Name and ID"/>
+                        <input className="form__box" type="text" name="name" placeholder="Name and ID" />
                     </div>
 
                     <div className="form__row-two">
                         <div className="box">
                             <h4 className="form__label">ADDRESS</h4>
-                            <input className="form__box" type="text" name="address" placeholder="Enter Address"/>
+                            <input className="form__box" type="text" name="address" placeholder="Enter Address" />
                         </div>
 
                         <div className="box">
@@ -51,36 +52,36 @@ export default class NewLocation extends Component {
                     <div className="form__row-three">
                         <div className="box">
                             <h4 className="form__label">CONTACT NAME</h4>
-                            <input className="form__box" type="text" name="contactname" placeholder="Enter Name"/>
+                            <input className="form__box" type="text" name="contactname" placeholder="Enter Name" />
                         </div>
                         <div className="box">
                             <h4 className="form__label">POSITION</h4>
-                            <input className="form__box" type="text" name="position" placeholder="Enter Position"/>
+                            <input className="form__box" type="text" name="position" placeholder="Enter Position" />
                         </div>
                     </div>
 
                     <div className="form__row-four">
                         <div className="box">
                             <h4 className="form__label">PHONE NUMBER</h4>
-                            <input className="form__box"type="text" name="phone" placeholder="(000)- 000- 000"/>
+                            <input className="form__box" type="text" name="phone" placeholder="(000)- 000- 000" />
                         </div>
-                        
+
                         <div className="box">
                             <h4 className="form__label">EMAIL</h4>
-                            <input className="form__box"type="text" name="email" placeholder="email@instock.com"/>
+                            <input className="form__box" type="text" name="email" placeholder="email@instock.com" />
                         </div>
                     </div>
 
                     <div className="form__row-five">
                         <div className="box">
                             <h4 className="description">CATEGORIES</h4>
-                            <input type="text" name="categories" placeholder="Use commas to separate each category"/>
+                            <input type="text" name="categories" placeholder="Use commas to separate each category" />
                         </div>
                     </div>
-                <div className="btn-container">
-                    <button className="btn-cancel">CANCEL</button>
-                    <button className="btn-save">SAVE</button>
-                </div>
+                    <div className="btn-container">
+                        <button type="button" className="btn-cancel" onClick={() => this.props.history.goBack()}>CANCEL</button>
+                        <button className="btn-save">SAVE</button>
+                    </div>
                 </form>
             </>
         );
